c3fde20ea9f129df4d78b2a7a672fad60a55c7d5
the-Fish2 <nishkamk2@gmail.com>
2024-06-24 21:24:10 -0700

    updated goals

--- js/main.js
+++ js/main.js
@@ -7,10 +7,12 @@ inputDataManager._buildForm();
 
 /* NEXT STEPS: 
 5) DONE - set up flexible dimensions
-6) add in title feature (for future links), add words to bars (basically just cleaning it up to make it more like connections)
+6) DONE - add words to bars (basically just cleaning it up to make it more like connections)
+7) add in title feature (for future links)
+8) allow un-click for buttons
 1) DONE - set up data manager to make OOP a bit better
 2) DONE - clear bars on resubmit
 4) DONE - randomize tile placement (as it stands the game is not very hard...)
-6) store previous connections - static database that i only push to
-7) make pretty like title :)
+10) store previous connections - static database that i only push to
+9) container-ize kinda - have different sections for form submit, play connections, past games
 */
